698957a48c7d771c535ac72e32866288f35d59a1
Nakshatra Goel <139595090+Nakshatra05@users.noreply.github.com>
2024-07-02 15:04:10 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -139,7 +139,7 @@ const App: React.FC = () => {
               <Hamster size={24} className="text-[#d4d4d4]" />
             </div>
             <div>
-              <p className="text-sm">Nikandr (CEO)</p>
+              <p className="text-sm"> Nakshatra (CEO)</p>
             </div>
           </div>
           <div className="flex items-center justify-between space-x-4 mt-1">
